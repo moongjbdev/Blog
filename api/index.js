f2cfb37ebbeedc76f42b080acb8ac61317f08e6b
moongjbdev <moongjbdev@gmail.com>
2024-06-23 14:26:12 +0700

    User route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/userRoute.js";
 //=====================================================================//
+//Config
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -12,4 +13,9 @@ const URI_DB = process.env.URI_DB || "mongodb://localhost:27017";
 mongoose.connect(URI_DB).then(() => console.log("Connected to MongoDB, Successfully!")).catch(err => console.log(err));;
 const app = express();
 
-app.listen(3000, () => {console.log(`Server is running on port ${PORT}`)});
+// Routes
+app.use('/api/user', userRoutes);
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
