71d1fbd4a3d86da34f4ba73429deb4c428969563
moongjbdev <moongjbdev@gmail.com>
2024-06-27 15:57:11 +0700

    update header with user data

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -15,9 +15,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    profilePicture: {
+    profilePicture: {       
         type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw14GcrDgf67iZTrAquK8PRp&ust=1719560999731000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjPsYam-4YDFQAAAAAdAAAAABAE",
+        default: "" //Mac dinh khong de gi ca
     }
 },{timestamps: true}
 );
