4f0d390cf89cfb10e04d7af600b10d40ef9671db
moongjbdev <moongjbdev@gmail.com>
2024-06-28 22:06:27 +0700

    sidebar

--- web/src/pages/DashBoard.jsx
+++ web/src/pages/DashBoard.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function DashBoard() {
-  return <div>DashBoard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    setTab(tabFromUrl); // Update tab state based on query parameter
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      <div className="content">
+        {/* Profile */}
+        {tab === "profile" && <DashProfile />}
+        {/* Add more conditions for other tabs if needed */}
+      </div>
+    </div>
+  );
 }
