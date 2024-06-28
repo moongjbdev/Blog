05a249353c1e5bad829a2ded94ab6513a1b85425
moongjbdev <moongjbdev@gmail.com>
2024-06-29 02:53:15 +0700

    complate image upload

--- web/src/components/DashSidebar.jsx
+++ web/src/components/DashSidebar.jsx
@@ -22,6 +22,7 @@ export default function DashSidebar() {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div" //cancer double a tag
             >
               Profile
             </Sidebar.Item>
