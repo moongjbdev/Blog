46045023e3a66465e179077e91b6879e2ed3ad6d
moongjbdev <moongjbdev@gmail.com>
2024-06-30 14:20:56 +0700

    create post

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,24 +1,30 @@
-import Post from "../models/postModel.js"
-import { errorHandle } from "../utils/error.js"
+import Post from "../models/postModel.js";
+import { errorHandle } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-    //check if user is admin => next
-    if (!req.user.isAdmin) {
-        return next(errorHandle(401, "You are not allowed to create post"))
-    }
-    if(!req.body.title || !req.body.content ) {
-        return next(errorHandle(400, "Please fill all the fields"))
-    }
-    const slug = req.body.title.split(" ").join("-").toLowerCase().replace(/[^a-zA-Z0-9]/g, "-")
-    const newPost = new Post({
-        ...req.body,
-        slug,
-        userId: req.user._id
-    })
-    try {
-        const savePost = await newPost.save()
-        res.status(201).json(savePost)
-    } catch (error) {
-        next(error)
-    }
-}
+  //check if user is admin => next
+  if (!req.user.isAdmin) {
+    return next(errorHandle(401, "You are not allowed to create post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandle(400, "Please fill all the fields"));
+  }
+  const slug = req.body.title
+    .toLowerCase() // Convert to lowercase
+    .split(" ") // Split by spaces
+    .join("-") // Join with hyphens
+    .replace(/[^a-z0-9-]/g, "") // Remove all non-alphanumeric characters except hyphens
+    .trim()
+
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user._id,
+  });
+  try {
+    const savePost = await newPost.save();
+    res.status(201).json(savePost);
+  } catch (error) {
+    next(error);
+  }
+};
